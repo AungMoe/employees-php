87c7c7bcd2e8e19718ff998b698a073a88f42551
Aung Hlaing Moe <aunghlaingmoe.taunggyi@gmail.com>
2021-03-30 06:53:14 +0700

    Add files via upload

--- includes/connect.php
+++ includes/connect.php
@@ -1,20 +1,20 @@
 <?php
-// session_start();
-// $servername = "localhost";
-// $server_user = "root";
-// $server_pass = "";
-// $dbname = "food";
-// $name = $_SESSION['name'];
-// $role = $_SESSION['role'];
-// $con = new mysqli($servername, $server_user, $server_pass, $dbname);
-
 session_start();
-$servername = "remotemysql.com";
-$server_user = "HRyxs0JYts";
-$server_pass = "sOrJWv38vD";
-$dbname = "HRyxs0JYts";
+$servername = "localhost";
+$server_user = "root";
+$server_pass = "";
+$dbname = "food";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
+
+// session_start();
+// $servername = "remotemysql.com";
+// $server_user = "HRyxs0JYts";
+// $server_pass = "sOrJWv38vD";
+// $dbname = "HRyxs0JYts";
+// $name = $_SESSION['name'];
+// $role = $_SESSION['role'];
+// $con = new mysqli($servername, $server_user, $server_pass, $dbname);
 
 
